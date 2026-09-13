1b36d47482012fe323142b88686e90c14dc36ef5
Lucas Faria <lucasfernandofaria@gmail.com>
2026-09-12 22:34:29 -0300

    correção do desafio 44

--- desafios/desafio44.py
+++ desafios/desafio44.py
@@ -10,8 +10,8 @@ if opcao == 1:
     print(f'Para pagamento à vista, o total fica R${float(preco-(preco/100*10))}, por que teve R${float(preco/100*10)} de desconto!')
 elif opcao == 2:
     parcelas = int(input('Em quantas vezes deseja fazer? '))
-    elif parcelas == 1:
-    print(f'Para gamento à vista no cartão, o total fica R${float(preco-(preco/100*5))}, por que teve R${float(preco/100*5)} de desconto!')
+    if parcelas == 1:
+        print(f'Para gamento à vista no cartão, o total fica R${float(preco-(preco/100*5))}, por que teve R${float(preco/100*5)} de desconto!')
     elif parcelas == 2:
         print(f'Para pagamento em até 2 vezes no cartão o total fica R${preco}!')
     elif parcelas >= 3:
